e16472b01370faf817a7272e54afc88ceb14458c
Truman Purnell <truman.purnell@gmail.com>
2018-09-28 23:09:18 -0700

    REALITY CHECK : READ BYTES LIKE YOU WANT EM'

--- index.mjs
+++ index.mjs
@@ -1,17 +1,24 @@
-class ByteReader {
+
+export default class ByteReader {
     constructor(props) {
-        this.data = props.data
+        this.bytes = props.bytes
         this.head = props.head || 0
         this.format = props.format
+        this.base = props.base
     }
 
-    isEmpty() { return this.head >= this.data.length }
-
-    peek() { return this.data[this.head] }
-
-    * read(bytes = 1) {
-        while (bytes--)
-            yield this.data[this.head++]
-    }
-
+    isEmpty() { return this.head >= this.bytes.length }
+    peek() { return this.bytes[this.head] }
+    read(bytes = 1) { this.bytes[head++] }
 }
+
+import fs from 'fs'
+
+const file = fs.readFileSync('/Users/tru/Desktop/.../movies/ed/calculus-strang/The\ Exponential\ Function-oo1ZZlvT2LQ.webm')
+
+console.time('1')
+
+for(let i=0; i< file.length; i++)
+    file[0] + 1
+
+console.timeEnd('1')
