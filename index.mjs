8486762fb9bde0c35e5a71a82833bc7f6bc91a40
Truman Purnell <truman.purnell@gmail.com>
2018-09-30 13:26:17 -0700

    leading zeros fn time tested

--- index.mjs
+++ index.mjs
@@ -5,8 +5,20 @@ export default class ByteReader {
         this.head = 0
     }
 
+    static leadingZeros(byte) {
+        if (byte > 128) return 0
+        if (byte > 64) return 1
+        if (byte > 32) return 2
+        if (byte > 16) return 3
+        if (byte > 8) return 4
+        if (byte > 4) return 5
+        if (byte > 2) return 6
+        if (byte > 1) return 7
+    }
+
     read() { return this.file[this.head++] } // twice as slow as direct access
     isEmpty() { return this.head >= this.file.length }
     peek() { return this.file[this.head] }
 
 }
+
