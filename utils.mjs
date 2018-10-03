32684148d2f928c162525a3a1d275e1af605ef06
Truman Purnell <truman.purnell@gmail.com>
2018-10-02 22:04:57 -0700

    removes marker logic and moves leading zeros to utils

---
+++ utils.mjs
@@ -0,0 +1,16 @@
+
+class ByteUtils {
+    static leadZ(byte) {
+        if (byte >= 128) return 0
+        if (byte >= 64) return 1
+        if (byte >= 32) return 2
+        if (byte >= 16) return 3
+        if (byte >= 8) return 4
+        if (byte >= 4) return 5
+        if (byte >= 2) return 6
+        if (byte >= 1) return 7
+        
+        return 8
+    }
+}
+
