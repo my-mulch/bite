36da73cdf159042abb1c0a10b8fceb3e56b82072
Truman Purnell <truman.purnell@gmail.com>
2018-10-02 22:33:53 -0700

    adds extract and advance to support EBML parsing

--- index.mjs
+++ index.mjs
@@ -10,6 +10,10 @@ export default class ByteReader {
     read() { return this.bytes[this.head++] } // twice as slow as direct access
     isEmpty() { return this.head >= this.bytes.length }
     peek() { return this.bytes[this.head] }
+
+    extract(vint) { return this.bytes.slice(...vint) }
+    advance(marker) { return [this.head, this.head += marker] }
+
 }
 
 ByteReader.utils = ByteUtils
